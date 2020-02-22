aef032a98540b3d9f02a06c298c4c6443b10bef4
soleni <k1a3w3a7i1@gmail.com>
2020-02-22 15:03:43 +0300

    feat: add main

--- src/main.c
+++ src/main.c
@@ -1,73 +1,32 @@
-#include <windows.h>
+#include "header.h"
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int main(int argc, char** argv){
+	char BinaryFileName[50];
+	char FileName[50];
+	char payment[8];
+	char LineNumber[8];
 
-int WINAPI WinMain(HINSTANCE hInst,
-                   HINSTANCE hPrevInst,
-                   PSTR szCmdLine,
-                   int iCmdShow) {
-    static TCHAR szAppName[] = TEXT("HelloWin");
-    HWND hwnd;
-    MSG msg;
-    WNDCLASS wndclass;
-    wndclass.style = CS_HREDRAW | CS_VREDRAW;
-    wndclass.lpfnWndProc = WndProc;
-    wndclass.cbClsExtra = 0;
-    wndclass.cbWndExtra = 0;
-    wndclass.hInstance = hInst;
-    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hbrBackground =
-        (HBRUSH) GetStockObject(WHITE_BRUSH);
-    wndclass.lpszMenuName = NULL;
-    wndclass.lpszClassName = szAppName;
-    if (!RegisterClass(&wndclass)) {
-        MessageBox(NULL,
-                   TEXT("This program requires Windows NT."), "msg", 0);
-        return 0;
-    }
-    hwnd = CreateWindow(szAppName,
-                        TEXT("The Hello Program"),
-                        WS_OVERLAPPEDWINDOW,
-                        CW_USEDEFAULT,
-                        CW_USEDEFAULT,
-                        CW_USEDEFAULT,
-                        CW_USEDEFAULT,
-                        NULL,
-                        NULL,
-                        hInst,
-                        NULL);
-    ShowWindow(hwnd, iCmdShow);
-    UpdateWindow(hwnd);
-    { while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-	DispatchMessage(&msg);
-    }
-    return msg.wParam;
-}
+	printf("Enter binary file name: ");
+	scanf("%49s", BinaryFileName);
+	printf("Enter line number: ");
+	scanf("%7s", LineNumber);
+	
+	char cmd[100];
+	sprintf(cmd, "creator.exe %s %s", BinaryFileName, LineNumber);
+	STARTUPINFO info = {sizeof(info)};
+	PROCESS_INFORMATION processInfo;
+	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
+	WaitForSingleObject(processInfo.hProcess, INFINITE);
 
-LRESULT CALLBACK WndProc(HWND hwnd,
-                         UINT message,
-                         WPARAM wParam,
-                         LPARAM lParam) {
-    HDC hdc;
-    PAINTSTRUCT ps;
-    RECT rect;
-    switch (message) {
-        case WM_CREATE:
-            return 0;
-        case WM_PAINT:
-            hdc = BeginPaint(hwnd, &ps);
-            GetClientRect(hwnd, &rect);
-            DrawText(hdc,
-                     TEXT("Hello, Windows!"),
-                     -1, &rect,
-                     DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-            EndPaint(hwnd, &ps);
-            return 0;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-    }
-    return DefWindowProc(hwnd, message, wParam, lParam);
+	printf("Enter report file name: ");
+	scanf("%49s", FileName);
+	printf("Enter payment value: ");
+	scanf("%7s", payment);
+
+	sprintf(cmd, "reporter.exe %s %s %s", BinaryFileName, FileName, payment);
+	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
+	WaitForSingleObject(processInfo.hProcess, INFINITE);
+
+	printf("Finished!\n");
+	return 0;
 }
