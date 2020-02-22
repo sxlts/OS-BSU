465bfaa0a13d84cf2ecfd8f07938126a5943d7ce
soleni <k1a3w3a7i1@gmail.com>
2020-02-22 15:40:06 +0300

    fix: atoi() -> atof()

--- src/reporter.c
+++ src/reporter.c
@@ -4,11 +4,11 @@ int main(int argc, char** argv){
 	if(argc < 4) exit (1);
 	
 	FILE *InputFile = fopen(argv[1], "rb");
-	FILE *OutputFile = fopen(argv[2], "w+a");
+	FILE *OutputFile = fopen(argv[2], "w+");
 	if(!InputFile) exit(1);
 	if(!OutputFile) exit(1);
 
-	double pay = atoi(argv[3]);
+	double pay = atof(argv[3]);
 	EMPLOYEE temp;
 
 	printf("%-10s%-10s%-10s\n", "ID:", "Name:", "Salary:");
@@ -16,6 +16,7 @@ int main(int argc, char** argv){
 		printf("%-10d%-10s%-8.1lf\n", temp.num, temp.name, temp.hours * pay);
 		fprintf(OutputFile, "%-10d%-10s%-8.1lf\n", temp.num, temp.name, temp.hours * pay);
 	}
+	printf("Report added to %s !\n", argv[2]);
 
 	fclose(InputFile);
 	fclose(OutputFile);
