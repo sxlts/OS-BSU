9709acb7bba8117f72cd5639e2e61121373fd3f0
soleni <k1a3w3a7i1@gmail.com>
2020-02-22 15:10:07 +0300

    style: improve reading

--- src/main.c
+++ src/main.c
@@ -1,31 +1,56 @@
 #include "header.h"
 
+bool CALL(char *cmd){
+	STARTUPINFO info = {sizeof(info)};
+	PROCESS_INFORMATION processInfo;
+	CreateProcess(
+		NULL,
+		cmd,
+		NULL,
+		NULL,
+		TRUE,
+		0,
+		NULL,
+		NULL,
+		&info,
+		&processInfo
+	);
+	WaitForSingleObject(processInfo.hProcess, INFINITE);
+}
+
 int main(int argc, char** argv){
 	char BinaryFileName[50];
 	char FileName[50];
-	char payment[8];
-	char LineNumber[8];
+	double payment;
+	int LineNumber;
 
 	printf("Enter binary file name: ");
 	scanf("%49s", BinaryFileName);
 	printf("Enter line number: ");
-	scanf("%7s", LineNumber);
+	scanf("%d", LineNumber);
 	
 	char cmd[100];
-	sprintf(cmd, "creator.exe %s %s", BinaryFileName, LineNumber);
-	STARTUPINFO info = {sizeof(info)};
-	PROCESS_INFORMATION processInfo;
-	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
-	WaitForSingleObject(processInfo.hProcess, INFINITE);
+	sprintf(
+		cmd,
+		"creator.exe %s %d",
+		BinaryFileName,
+		LineNumber
+	);
+	CALL(cmd);
 
 	printf("Enter report file name: ");
 	scanf("%49s", FileName);
 	printf("Enter payment value: ");
-	scanf("%7s", payment);
+	scanf("%lf", payment);
 
-	sprintf(cmd, "reporter.exe %s %s %s", BinaryFileName, FileName, payment);
-	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
-	WaitForSingleObject(processInfo.hProcess, INFINITE);
+	sprintf(
+		cmd,
+		"reporter.exe %s %s %lf",
+		BinaryFileName,
+		FileName,
+		payment
+	);
+	CALL(cmd);
 
 	printf("Finished!\n");
 	return 0;
